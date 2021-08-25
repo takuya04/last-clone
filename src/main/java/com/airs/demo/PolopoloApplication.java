6fddb2c8f314bdcbdcffc49105ee998ffe510e9d
takuya04 <takuya.ct0418@gmail.com>
2021-08-25 16:24:10 +0900

    1st

---
+++ src/main/java/com/airs/demo/PolopoloApplication.java
@@ -0,0 +1,13 @@
+package com.airs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolopoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolopoloApplication.class, args);
+	}
+
+}
